394b9f405cd1f5c2ca88e32de0c81d3751e500f9
Courtney Webster <courtney@atlmd.net>
2022-06-22 18:35:54 -0400

    adding login-view and hooks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,79 +10,59 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: '6286d1815bb1ae9a04c36b1c',
-          Title: 'The Crimson Pirate',
-          Description: 'During the 1700s, pirate Captain Vallo seizes a British warship and gets involved in various money-making schemes involving Caribbean rebels led by El Libre, British envoy Baron Jose Gruda, and a beautiful courtesan named Consuelo.',
-          Director: {
-            Name: 'Robert Siodmak',
-            Bio: 'Robert Siodmak was a German director, writer and producer.',
-            Birth: '1900',
-            Death: '1973'
-          },
-          Genre: {
-            Name: 'Comedy',
-            Description: 'A comedy is a category in which emphasizes humor. It is designed to make the audience laugh through amusement.'
-          },
-          ImagePath: 'https://m.media-amazon.com/images/M/MV5BYTVjMDM2NGMtYjE0Ny00MTQ5LTg4MmUtNDU0ZGY3NGQ0OTA3XkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_.jpg',
-          Featured: true
-        },
-        {
-          _id: '6286d58b5bb1ae9a04c36b1d',
-          Title: 'Captain Blood',
-          Description: 'After treating a Monmouth rebel against King James II in 1680s England, a young Irish doctor is exiled as a slave to Jamaica where he captures a Spanish gelleon and becomes the most feared pirate of the Caribbean.',
-          Director: {
-            Name: 'Michael Curtiz',
-            Bio: "Michael Curtiz was a Hungarian director, writer and actor.",
-            Birth: '1886',
-            Death: '1962'
-          },
-          Genre: {
-            Name: 'History',
-            Description: 'Historical film is a fiction film showing past events or set within a historical period.'
-          },
-          ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjMwNDc1MzMyNV5BMl5BanBnXkFtZTgwODUzMjU1MzE@._V1_.jpg',
-          Featured: false
-        },
-        {
-          _id: '6286e6445bb1ae9a04c36b25',
-          Title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
-          Description: 'Blacksmith Will Turner teams up with an eccentric pirate named Jack Sparrow to save his love from undead pirates.',
-          Director: {
-            Name: 'Gore Verbinski',
-            Bio: "Gore Verbinski is an American producer and director.",
-            Birth: '1964'
-          },
-          Genre: {
-            Name: 'Fantasy',
-            Description: 'Fantasy films include fantastic themes like magic, suppernatural events, mythology and folklore.'
-          },
-          ImagePath: 'https://img1.hotstarext.com/image/upload/f_auto,t_vl/sources/r1/cms/prod/4452/674452-v',
-          Featured: false
-        },
-      ],
+      movies: [],
       selectedMovie: null
-    };
+    }
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  componentDidMount() {
+    axios.get('https://piratemoviesapi.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
-    if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    if (movies.length === 0) return <div className='main-view'>The list is empty!</div>;
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      <div className='main-view'>
-        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
+      <div className="main-view">
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+          ))
+        }
       </div>
     );
   }
+
 }
